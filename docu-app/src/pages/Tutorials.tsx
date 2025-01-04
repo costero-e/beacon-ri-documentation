9369f47c53464b9a3166fbb17029ddcec94d046c
Giulia <giulia.cellerino@icloud.com>
2025-01-04 19:22:04 +0700

    Fixed pagination of Tutorials

--- docu-app/src/pages/Tutorials.tsx
+++ docu-app/src/pages/Tutorials.tsx
@@ -4,19 +4,17 @@ import "./BeaconNetworkUI/NetworkUIQueries.css";
 const Tutorials: React.FC = () => {
   return (
     <div className="networkUIQueriesContainer">
-      <div className="networkUIQueriesContainer">
-        <h2 className="user-path">
-          Documentation
-          <img
-            src="arrow-right-bold.svg"
-            alt="arrow right"
-            className="arrow-icon"
-          />
-          <span className="user-path-title">Tutorials</span>
-        </h2>
-        <h1>Tutorials</h1>
-        <h4>Coming soon...</h4>
-      </div>
+      <h2 className="user-path">
+        Documentation
+        <img
+          src="arrow-right-bold.svg"
+          alt="arrow right"
+          className="arrow-icon"
+        />
+        <span className="user-path-title">Tutorials</span>
+      </h2>
+      <h1>Tutorials</h1>
+      <h4>Coming soon...</h4>
     </div>
   );
 };
