ee071620ef7a1ac69f016772f65d0c1c6fe509db
costero-e <orildsaga@gmail.com>
2025-05-30 11:36:30 +0200

    adding absolute paths for images

--- docu-app/src/pages/Beacon2RITools/CommonErrors.tsx
+++ docu-app/src/pages/Beacon2RITools/CommonErrors.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ConfigFileTools.css";
 import copyIcon from "../../assets/copy-symbol.svg";
-import copyIconError from "../..assets/copyIconError.svg";
+import copyIconError from "../../assets/copyIconError.svg";
 import OnThisPage from "../../components/OnThisPage";
 
 const CommonErrors: React.FC = () => {
